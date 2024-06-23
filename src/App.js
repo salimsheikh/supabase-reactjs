0d950ac9705ce8c9baa077d383b51cc25c92fc98
salimsheikh <salimsheikh4u2000@gmail.com>
2024-06-24 02:58:22 +0530

    rows, insert, delete, update completed

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import { supabase } from './lib/helper/supabase_client'
 
 export default function App() {
 
+  const inputRef = useRef();
   //const [email, setEmail] = useState('');
   //const [password, setPassword] = useState('');
   const [session, setSession] = useState(null);
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(null);  
+  const [error, setError] = useState(null);
+  const [todo, setTodo] = useState([]);
 
     const login = async () => {
 
@@ -30,6 +33,10 @@ export default function App() {
 
     useEffect(() => {
       async function fetchSession() {
+
+        getTodo();
+        console.log("useEffect");
+
         const { data: { session }, error } = await supabase.auth.getSession();
         if (error) {
           console.error('Error fetching session:', error);
@@ -66,29 +73,100 @@ export default function App() {
       } catch (error) {
         console.error('Error during sign up:', error);
       }
-    }
-
-    const inputRef = useRef();
-    const {error, setError} = useState(null);
+    }   
 
     const handleCreateTody = async () => {
       const title = inputRef.current.value;
-        const response = await supabase.from('todo').insert({title:title, user_id: user.id}).select("*").single();
-        console.log(response);
+      const response = await supabase.from('todo').insert({title:title, user_id: user.id}).select("*").single();
+      
+      setError(response?.error);
+      
+      if(response.data){
+        setTodo((currentTodo) => [...currentTodo, response.data]);
+      }
+    }
+
+    const getTodo = async () => {
+      const response = await supabase.from("todo").select("*");
+
+      setError(response?.error);
+      setTodo(response?.data);
+      console.log(response);
+    }
+
+    const handleUpdate = async (id) => {
+      const response = await supabase.from("todo").update({complete:true}).eq("id",id).select("*").single();      
+      setError(response?.error);
+
+      if(!response.error){
+        setTodo((currentTodo) => currentTodo.map((todo) => {
+          if(todo.id === id){
+              todo.complete = true;
+          }
+          return todo;
+        }))
+      }
+    }
+
+    const handleDelete = async (id) =>{
+      const response = await supabase.from("todo").delete().eq("id",id).select("*").single();
+
+      console.log(response.data);
+
+      if(!response.error){
+        if(!response.error){
+          setTodo((currentTodo) => currentTodo.filter((todo) => todo.id !== id));
+        }
+      }
     }
 
     return (
       <div>
         <div>
           <h1>React Supabase database CRUD and policy</h1>
-          <div>
-              <input ref={inputRef} />
-              <button onClick={handleCreateTody} > add</button>
+          {user? (
+              <div>
+                <div>
+                  <input ref={inputRef} maxLength="10" style={{marginRight: 5}} />
+                  <button onClick={handleCreateTody} > add</button>
+                  {error && <pre>error.message</pre>}
+                </div>
 
-              {error && <pre>error.message</pre>}
-
-          </div>
-        </div>      
+                <table>
+                  <tbody>
+                    {todo && 
+                      todo.map(
+                        (value, index) => {
+                          const text_color = value.complete === true ? 'green' : 'none';
+                          return (
+                            <tr key={index}>
+                                <td style={{color:text_color}}>{value.title}</td>
+                                <td><button onClick={()=>handleUpdate(value.id)}>Complete</button></td>
+                                <td><button onClick={()=>handleDelete(value.id)}>Delete</button></td>
+                            </tr>
+                          );
+                        }
+                      )
+                    }
+                  </tbody>
+                 </table>
+              </div>
+          ):(
+            <div></div>
+          )
+        }          
+        </div>
+        
+        <div>
+        <br />
+          {user ? 
+            (
+              <button onClick={logout} >Logout</button>
+            ):(
+              <button onClick={login} >Login</button>
+            )
+          }
+        </div>
       </div>
     )  
   }
